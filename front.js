86e0a7e15a284ba8fe61c29e19ea8ec17547d1b5
Yuki Nakato <crystal.icicle@gmail.com>
2023-11-12 15:25:29 +0900

    multiple

--- front.js
+++ front.js
@@ -1,20 +1,32 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  switch (request.operation) {
-    case 'downloadObject':
-      const downloadButton = document.getElementById('download-object-button')
-      downloadButton.click()
+async function sleep() {
+  return new Promise((resolve) => setTimeout(resolve, 50))
+}
 
-      const inputs = document.getElementsByTagName('input')
-      for (const input of inputs) {
-        if (input.type === 'checkbox' && input.checked) {
-          input.click()
-        }
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  switch (request.operation) {
+    case 'downloadObject': {
+      const downloadButton = document.getElementById('download-object-button')
+      const targetInputs = [...document.getElementsByTagName('input')].filter((el) => el.type === 'checkbox' && el.checked)
+
+      for (const input of targetInputs) {
+        input.click()
+        await sleep()
+      }
+
+      for (const input of targetInputs) {
+        input.click()
+        await sleep()
+        downloadButton.click()
+        await sleep()
+        input.click()
+        await sleep()
       }
 
       sendResponse()
       break
-
-    default:
+    }
+    default: {
       sendResponse()
+    }
   }
 })
