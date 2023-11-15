0bd0b9657795e670bb5c809a20ef58443f0bd217
Yuki Nakato <crystal.icicle@gmail.com>
2023-11-15 18:47:36 +0900

    update

--- front.js
+++ front.js
@@ -6,14 +6,23 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   switch (request.operation) {
     case 'downloadObject': {
       const downloadButton = document.getElementById('download-object-button')
-      const targetInputs = [...document.getElementsByTagName('input')].filter((el) => el.type === 'checkbox' && el.checked)
 
-      for (const input of targetInputs) {
-        input.click()
+      const allCheckInput = document.getElementsByTagName('thead')[0].getElementsByTagName('input')[0]
+      const checkedFileInputs = [...document.getElementsByTagName('tbody')[0].getElementsByTagName('input')].filter(
+        (el) => el.type === 'checkbox' && el.checked
+      )
+
+      if (allCheckInput.checked) {
+        allCheckInput.click()
         await sleep()
+      } else {
+        for (const input of checkedFileInputs) {
+          input.click()
+          await sleep()
+        }
       }
 
-      for (const input of targetInputs) {
+      for (const input of checkedFileInputs) {
         input.click()
         await sleep()
         downloadButton.click()
